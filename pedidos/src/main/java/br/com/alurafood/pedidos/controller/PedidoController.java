050f03e6b5700cf36c9fcbfb16dfdef78ae9da1e
Daniel Souza <workdndsza@gmail.com>
2024-08-01 13:02:20 -0300

    configurando para ter loadbalancer no pedido ms

--- pedidos/src/main/java/br/com/alurafood/pedidos/controller/PedidoController.java
+++ pedidos/src/main/java/br/com/alurafood/pedidos/controller/PedidoController.java
@@ -1,17 +1,27 @@
 package br.com.alurafood.pedidos.controller;
 
-import br.com.alurafood.pedidos.dto.PedidoDto;
-import br.com.alurafood.pedidos.dto.StatusDto;
-import br.com.alurafood.pedidos.service.PedidoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
+import java.net.URI;
+import java.util.List;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-import java.net.URI;
-import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import br.com.alurafood.pedidos.dto.PedidoDto;
+import br.com.alurafood.pedidos.dto.StatusDto;
+import br.com.alurafood.pedidos.service.PedidoService;
+
 
 @RestController
 @RequestMapping("/pedidos")
@@ -57,4 +67,10 @@ public class PedidoController {
             return ResponseEntity.ok().build();
 
         }
+
+        @GetMapping("/porta")
+        public String retornaPorta(@Value("${local.server.port}") String porta){
+            return String.format("Requisição respondida pela instância executando na porta %s", porta);
+        }
+
 }
